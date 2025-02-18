26429d0f27cfae3c57c8abdf2b52574b2040a980
ANNGLORIOUS <mueniannglorious@gmail.com>
2025-02-18 11:39:35 +0300

    updated the tailwind file

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#0A3D2E',       
+          accent: '#D4AF37',        
+          secondary: '#F5E1C8',     
+          darkContrast: '#2E2E2E',   
+          lightContrast: '#FAF3E0',  
+        },
+        fontFamily: {
+          heading: ['Playfair Display', 'serif'],  
+          body: ['Lora', 'serif'],                  
+          accent: ['Poppins', 'sans-serif'],      
+        },
+      },
+    },
+    plugins: [],
+  };
